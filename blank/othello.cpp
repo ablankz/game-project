3ae43ddfe0004cd807b7733b61c1bf881d3fd028
ablankz <hayaken3007@i.softbank.jp>
2021-11-04 04:29:42 +0900

    othello 3

--- blank/othello.cpp
+++ blank/othello.cpp
@@ -9,6 +9,8 @@ int turn_select(char player[]);
 void confirm(int* flag);
 int game(char py1[],char py2[]);
 void display(char py1[],char py2[]);
+int whole_check(int check_board[][2],int turn);
+int single_check(int i,int j,int turn,int signal);
 
 //othello board
 int board[9][9]={
@@ -16,8 +18,8 @@ int board[9][9]={
     {100,0,0,0,0,0,0,0,0},
     {100,0,0,0,0,0,0,0,0},
     {100,0,0,0,0,0,0,0,0},
-    {100,0,0,0,1,2,0,0,0},
     {100,0,0,0,2,1,0,0,0},
+    {100,0,0,0,1,2,0,0,0},
     {100,0,0,0,0,0,0,0,0},
     {100,0,0,0,0,0,0,0,0},
     {100,0,0,0,0,0,0,0,0},
@@ -153,11 +155,225 @@ void multi_mode(void){
  * @return py1 win -> 0 py2 win ->1 draw -> 2
 */
 int game(char py1[],char py2[]){
-    int winner=0;
+    int winner=0,able_check[64][2],able_num,v_num,h_num,flag=1;
     //display
     display(py1,py2);
 
+    printf("%s::turn\n",py1);
+    able_num=whole_check(able_check,1);
+    printf("your_able_point >> %d\n",able_num);
+    for(int i=0;i<able_num;++i){
+            printf("(%d,%d) ",able_check[i][0],able_check[i][1]);
+    }
+    while(flag){
+        printf("\n\nPlease enter the position you want to place in the following format.\n");
+        printf("verticalNumber horizontalNumber(Ex.3 4)\n >>>");
+        scanf("%d %d",&v_num,&h_num);
+        printf("\nyour select ==>>>> (%d,%d)\n\n",v_num,h_num);
+        confirm(&flag);
+    }
+    printf("(%d %d)\n",v_num,h_num);
     return winner;
+}
+
+int whole_check(int check_board[][2],int turn){
+    int i,j,num=0;
+    for(i=1;i<=8;++i){
+        for(j=1;j<=8;++j){
+            if(single_check(i,j,turn,-1)){
+                check_board[num][0] = i;
+                check_board[num++][1] = j;
+            }
+        }
+    }
+    check_board[num][0]='\0';
+    check_board[num][1]='\0';
+    return num;
+}
+
+/*
+ * disable >> 0 : enable >> 1 
+*/
+int single_check(int i,int j,int turn,int signal){
+    int a,b,min_a,max_a,min_b,max_b,para[8];// clockDirection
+    int op_turn; 
+    int num=0,a_key,b_key,t_b_key,p,tool[8][2],result=0;
+
+    op_turn= (turn==1) ? 2:1;
+
+    if(signal<0){
+        if(board[i][j]){
+            return 0;
+        }
+
+        if(i==1){
+            min_a=i;
+            a_key=1;
+        }else{
+            min_a=i-1;
+            a_key=0;
+        }
+        if(j==1){
+            min_b=j;
+            b_key=1;
+            t_b_key=1;
+        }else{
+            min_b=j-1;
+            b_key=0;
+            t_b_key=0;
+        }
+
+        max_a = (i==8) ? i : i+1;
+        max_b = (j==8) ? j : j+1;
+
+        for(a=min_a;a<=max_a;++a,++a_key){
+            for(b=min_b;b<=max_b;++b,++b_key){
+                if(board[a][b]==op_turn){
+                    tool[num][0]=a;
+                    tool[num][1]=b;
+                    para[num++]=a_key*10+b_key;
+                }
+            }
+            b_key=t_b_key;
+        }
+        for(p=0;p<num;++p){
+            if(single_check(tool[p][0],tool[p][1],turn,para[p])){
+                result=1;
+                break;
+            }
+        }
+
+    }else{
+        switch(signal){
+            case 0:
+                //left-top
+                if(i==1) return 0;
+                if(j==1) return 0;
+                if(board[i-1][j-1]==op_turn){
+                    if(single_check(i-1,j-1,turn,signal)){
+                        return 1;
+                    }else{
+                        return 0;
+                    }
+                }else if(board[i-1][j-1]==turn){
+                    return 1;
+                }else{
+                    return 0;
+                }
+                break;
+            case 1:
+                //middle-top
+                if(i==1) return 0;
+                if(board[i-1][j]==op_turn){
+                    if(single_check(i-1,j,turn,signal)){
+                        return 1;
+                    }else{
+                        return 0;
+                    }
+                }else if(board[i-1][j]==turn){
+                    return 1;
+                }else{
+                    return 0;
+                }
+                break;
+            case 2:
+                //right-top
+                if(i==1) return 0;
+                if(j==8) return 0;
+                if(board[i-1][j+1]==op_turn){
+                    if(single_check(i-1,j+1,turn,signal)){
+                        return 1;
+                    }else{
+                        return 0;
+                    }
+                }else if(board[i-1][j+1]==turn){
+                    return 1;
+                }else{
+                    return 0;
+                }
+                break;
+            case 10:
+                //left-middle
+                if(j==1) return 0;
+                if(board[i][j-1]==op_turn){
+                    if(single_check(i,j-1,turn,signal)){
+                        return 1;
+                    }else{
+                        return 0;
+                    }
+                }else if(board[i][j-1]==turn){
+                    return 1;
+                }else{
+                    return 0;
+                }
+                break;
+            case 12:
+                //right-middle
+                if(j==8) return 0;
+                if(board[i][j+1]==op_turn){
+                    if(single_check(i,j+1,turn,signal)){
+                        return 1;
+                    }else{
+                        return 0;
+                    }
+                }else if(board[i][j+1]==turn){
+                    return 1;
+                }else{
+                    return 0;
+                }
+                break;
+            case 20:
+                //left-bottom
+                if(i==8) return 0;
+                if(j==1) return 0;
+                if(board[i+1][j-1]==op_turn){
+                    if(single_check(i+1,j-1,turn,signal)){
+                        return 1;
+                    }else{
+                        return 0;
+                    }
+                }else if(board[i+1][j-1]==turn){
+                    return 1;
+                }else{
+                    return 0;
+                }
+                break;
+            case 21:
+                //middle-bottom
+                if(i==8) return 0;
+                if(board[i+1][j]==op_turn){
+                    if(single_check(i+1,j,turn,signal)){
+                        return 1;
+                    }else{
+                        return 0;
+                    }
+                }else if(board[i+1][j]==turn){
+                    return 1;
+                }else{
+                    return 0;
+                }
+                break;
+            case 22:
+                //left-bottom
+                if(i==8) return 0;
+                if(j==8) return 0;
+                if(board[i+1][j+1]==op_turn){
+                    if(single_check(i+1,j+1,turn,signal)){
+                        return 1;
+                    }else{
+                        return 0;
+                    }
+                }else if(board[i+1][j+1]==turn){
+                    return 1;
+                }else{
+                    return 0;
+                }
+                break;
+            default:
+                exit(-1);
+        }
+    }
+    return result;
 }
 
 /*
